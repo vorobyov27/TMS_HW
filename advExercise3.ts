3d6dd04dc373d675582ecf4b62ce1cf2e39a6bb1
Alexander <vorobyov27@gmail.com>
2022-10-12 23:14:05 +0300

    fix ex3

--- advExercise3.ts
+++ advExercise3.ts
@@ -1,6 +1,8 @@
 // Advanced Exercise 3: write # pyramide via a cycle.
 let str: string = '';
-for (let i: number = 0; i<6; i++) {
-    str = str+'#'
-    console.log(str)
+let pyramidBottom: number = 6;
+
+for (let i: number = 0; i<pyramidBottom; i++) {
+    str += '#';
+    console.log(str);
 }
