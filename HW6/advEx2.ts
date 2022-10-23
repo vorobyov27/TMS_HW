d5225b18e7245e9470d4ca578a78e745b0a24112
Alexander <vorobyov27@gmail.com>
2022-10-23 21:49:43 +0300

    add second exersice

--- HW6/advEx2.ts
+++ HW6/advEx2.ts
@@ -12,7 +12,7 @@
 //     }
     let student1: object = {
         name: 'Polina',
-        age: 27
+        age: 27,
     };
     let student2: object = {
         name: 'Polina',
@@ -21,10 +21,14 @@
 
     function compareObj(obj1: object, obj2: object): boolean {
         let compareResults = true;
-        for (const key in obj1) {
-            if (obj1[key] !== obj2[key]) compareResults = false;
+        const arr1 = (Object.values(obj1));
+        const arr2 = (Object.values(obj2));
+        for (let i: number = 0; i<arr1.length; i++) {
+                if (arr1[i] !== arr2[i]) {
+                    compareResults = false;
+                }
         }
         return compareResults;
     }
 
-    console.log(compareObj(student1, student2));
+(!compareObj(student1, student2) || !compareObj(student2, student1))? console.log("Students are different!"): console.log("Students are the same!");
