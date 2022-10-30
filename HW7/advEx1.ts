4d5ecb79cb1c5d3ea7c99c3c6971fc87147ab1be
Alexander <vorobyov27@gmail.com>
2022-10-30 19:28:24 +0300

    optimise code

--- HW7/advEx1.ts
+++ HW7/advEx1.ts
@@ -7,12 +7,12 @@ let incomeWord: string = readlinePalindrom.question("Enter your word: ");
 
 function isPalindrom(str: string): boolean {
     let palindromFlag = true;
-    const leftPart = str.slice(0, Math.floor(str.length/2));
-    const rightPart = str.slice( -Math.floor(str.length/2));
-    console.log("leftPart = " + leftPart);
-    console.log("rightPart = " + rightPart);
-    for (let i = 0, j = rightPart.length-1; i <leftPart.length, j > -1; i++, j--){
-        if (leftPart[i] !== rightPart[j]) palindromFlag = false;
+    const mid = Math.floor(str.length/2);
+    for (let i = 0; i<mid; i++){
+        if (str[i] !== str[str.length-1-i]) {
+            palindromFlag =  false;
+            break;
+        }
     }
     return palindromFlag;
 }
