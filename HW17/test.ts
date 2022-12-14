dc14f0e41f55e700bdd7b8b091a25ba992cbed90
Alexander <vorobyov27@gmail.com>
2022-12-14 23:04:02 +0300

    add NaN checks, divide 0 check

--- HW17/test.ts
+++ HW17/test.ts
@@ -5,12 +5,18 @@ function testAdd (arg1: number, arg2: number, expected: number) {
     const actual = new Calculator([arg1,arg2], 'add').start()
     if(actual === expected) console.log(`Passed test for ADD ${arg1} + ${arg2} = ${expected}, OK `);
     //else throw new Error(`Failed test for ADD ${arg1}, ${arg2}, Incorrect result: ${actual} != expected ${expected}, FAIL`)
+    else if (isNaN(arg1)||isNaN(arg2)) {
+        console.log(`Incorrect inputs. Inputs should be number.`)
+    }
     else console.log(`Failed test for ADD ${arg1}, ${arg2}, Incorrect result: ${actual} != expected ${expected}, FAIL`)
 }
 
 function testSubstract (arg1: number, arg2: number, expected: number) {
     const actual = new Calculator([arg1,arg2], 'substract').start()
     if(actual === expected) console.log(`Passed test for SUBSTRACT ${arg1} - ${arg2} = ${expected}, OK `);
+    else if (isNaN(arg1)||isNaN(arg2)) {
+        console.log(`Incorrect inputs. Inputs should be number.`)
+    }
     //else throw new Error(`Failed test for SUBSTRACT ${arg1}, ${arg2}, Incorrect result: ${actual} != expected ${expected}, FAIL`)
     else console.log(`Failed test for SUBSTRACT ${arg1}, ${arg2}, Incorrect result: ${actual} != expected ${expected}, FAIL`)
 }
@@ -18,6 +24,9 @@ function testSubstract (arg1: number, arg2: number, expected: number) {
 function testMultiply (arg1: number, arg2: number, expected: number) {
     const actual = new Calculator([arg1,arg2], 'multiply').start()
     if(actual === expected) console.log(`Passed test for MULTIPLY ${arg1} * ${arg2} = ${expected}, OK `);
+    else if (isNaN(arg1)||isNaN(arg2)) {
+        console.log(`Incorrect inputs. Inputs should be number.`)
+    }
     //else throw new Error(`Failed test for MULTIPLY ${arg1}, ${arg2}, Incorrect result: ${actual} != expected ${expected}, FAIL`)
     else console.log(`Failed test for MULTIPLY ${arg1}, ${arg2}, Incorrect result: ${actual} != expected ${expected}, FAIL`)
 }
@@ -25,6 +34,9 @@ function testMultiply (arg1: number, arg2: number, expected: number) {
 function testDivide (arg1: number, arg2: number, expected: number) {
     const actual = new Calculator([arg1,arg2], 'divide').start()
     if(actual === expected) console.log(`Passed test for DIVIDE ${arg1} / ${arg2} = ${expected}, OK `);
+    else if (isNaN(arg1)||isNaN(arg2)|| arg2 === 0) {
+        console.log(`Incorrect inputs. Inputs should be number. arg2 cannot be 0`)
+    }
     //else throw new Error(`Failed test for DIVIDE ${arg1}, ${arg2}, Incorrect result: ${actual} != expected ${expected}, FAIL`)
     else console.log(`Failed test for DIVIDE ${arg1}, ${arg2}, Incorrect result: ${actual} != expected ${expected}, FAIL`)
 }
@@ -32,31 +44,34 @@ function testDivide (arg1: number, arg2: number, expected: number) {
 function testPower (arg1: number, arg2: number, expected: number) {
     const actual = new Calculator([arg1,arg2], 'power').start()
     if(actual === expected) console.log(`Passed test for POWER ${arg1} ** ${arg2} = ${expected}, OK `);
+    else if (isNaN(arg1)||isNaN(arg2)) {
+        console.log(`Incorrect inputs. Inputs should be number.`)
+    }
     //else throw new Error(`Failed test for POWER ${arg1}, ${arg2}, Incorrect result: ${actual} != expected ${expected}, FAIL`)
     else console.log(`Failed test for POWER ${arg1}, ${arg2}, Incorrect result: ${actual} != expected ${expected}, FAIL`)
 }
 
 testAdd(1,2,3);
 testAdd(0,0,0);
-testAdd(1,2,5);
-testAdd(0,1,0);
+testAdd(NaN,1,NaN);
+testAdd(1,NaN,NaN);
 
 testSubstract(5,2,3);
 testSubstract(0,0,0);
-testSubstract(10,1,2);
-testSubstract(0,1,1);
+testSubstract(NaN,1,NaN);
+testSubstract(5,NaN,NaN);
 
 testMultiply(2,3,6);
 testMultiply(0,1,0);
-testMultiply(2,3,10);
-testMultiply(0,0,1);
+testMultiply(NaN,3,NaN);
+testMultiply(1,NaN,NaN);
 
 testDivide(10,5,2);
 testDivide(0,2,0);
-testDivide(10,5,3);
-testDivide(0,10,1);
+testDivide(NaN,5,NaN);
+testDivide(10,0,NaN);
 
 testPower(2,3,8);
 testPower(2,0,1);
-testPower(2,3,10);
-testPower(10,0,2);
+testPower(NaN,3,NaN);
+testPower(10,NaN,NaN);
