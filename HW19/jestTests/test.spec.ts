657736ec75a6755ba4f2cf82e1935232b582c033
Alexander <vorobyov27@gmail.com>
2022-12-20 01:19:33 +0300

    fix tests - add all checks for request in one test

--- HW19/jestTests/test.spec.ts
+++ HW19/jestTests/test.spec.ts
@@ -2,9 +2,14 @@ import superagent from "superagent"
 import {createUser, registerUser, loginUser, userNameOnly, emptyUser, emailOnly} from "../testdata/user"
 
 describe("Api test suit. Positive", () => {
-    it("1. Request users - response state", async () => {
+    it("1. Request users.", async () => {
         const result = await superagent.get("https://reqres.in/api/users?page=2");
         expect(result.status).toBe(200);
+        expect(result.body).toHaveProperty("page");
+        expect(result.body).toHaveProperty("per_page");
+        expect(result.body).toHaveProperty("total");
+        expect(result.body).toHaveProperty("total_pages");
+        expect(result.body).toHaveProperty("data");
     });
 
     it("2. Check email for user with id = 10", async () => {
@@ -13,71 +18,51 @@ describe("Api test suit. Positive", () => {
         expect(resObj.data.email).toBe("byron.fields@reqres.in");
     });
 
-    it("3. Create user. POST request - correct answer code.", async () => {
-        const result = await superagent
-        .post("https://reqres.in/api/users")
-        .send(createUser)
-        .set('Content-Type','application/json');
-
-        expect(result.status).toBe(201);
-    });
-
-    it("4. Create user. POST request. User created with correct name", async () => {
+    it("3. Create user. POST request. User created with correct name", async () => {
         const result = await superagent
         .post("https://reqres.in/api/users")
         .send(createUser)
         .set('Content-Type','application/json');
 
         const resObj = JSON.parse(result.text);
+        expect(result.status).toBe(201);
         expect(resObj.name).toBe("morpheus");
     });
 
-    it("5. Registration - successful. Correct response code.", async () => {
+    it("4. Registration - successful.", async () => {
         const result = await superagent
         .post("https://reqres.in/api/register")
         .send(registerUser)
         .set('Content-Type','application/json');
 
         expect(result.status).toBe(200);
+        expect(result.body).toHaveProperty("token");
+        expect(result.body).toHaveProperty("id");
     });
 
-    it("6. Check single resource id", async () => {
+    it("5. Check single resource id.", async () => {
         const result = await superagent
         .get("https://reqres.in/api/unknown/2");
         const resObj = JSON.parse(result.text);
         expect(resObj.data.id).toBe(2);
     });
 
-    it("7. Login. Correct response", async () => {
+    it("6. Login.", async () => {
         const result = await superagent
         .post("https://reqres.in/api/login")
         .send(loginUser)
         .set('Content-Type','application/json');
 
         expect(result.status).toBe(200);
+        expect(result.body).toHaveProperty("token");
     });
 
-    it("8. Login. Token in response", async () => {
-        const result = await superagent
-        .post("https://reqres.in/api/login")
-        .send(loginUser)
-        .set('Content-Type','application/json');
-
-        expect(result.body).toHaveProperty("token")
-    });
-
-    it("9. List resource. Correct response code.", async () => {
+    it("7. List resource.", async () => {
         const result = await superagent
         .get("https://reqres.in/api/unknown");
 
         expect(result.status).toBe(200);
-    });
-
-    it("10. List resource. Correct response code.", async () => {
-        const result = await superagent
-        .get("https://reqres.in/api/unknown");
-
-        expect(result.body).toHaveProperty("data")
+        expect(result.body).toHaveProperty("data");
     });
 });
 
@@ -124,23 +109,17 @@ describe("Api test suit. Negative", () => {
         expect(resObj).not.toHaveProperty("job");
     });
 
-    it("7. Update user with empty fields. Correct response code.", async () => {
-        const result = await superagent.put("https://reqres.in/api/users/2")
-        .send(emptyUser)
-        .set('Content-Type','application/json');
-        expect(result.status).toBe(200);
-    });
-
-    it("8. Update user with empty fields. Check empty values in user properties.", async () => {
+    it("7. Update user with empty fields. Check empty values in user properties.", async () => {
         const result = await superagent.put("https://reqres.in/api/users/2")
         .send(emptyUser)
         .set('Content-Type','application/json');
         const resObj = JSON.parse(result.text);
+        expect(result.status).toBe(200);
         expect(resObj.name).toBe("");
         expect(resObj.job).toBe("");
     });
 
-    it("9. Register. Unsuccessful. Correct response code.", async () => {
+    it("8. Register. Unsuccessful. Correct response code.", async () => {
         const result = await superagent.post("https://reqres.in/api/register")
         .send(emailOnly)
         .set('Content-Type','application/json')
@@ -148,7 +127,7 @@ describe("Api test suit. Negative", () => {
         expect(result.status).toBe(400);
     });
 
-    it("10. Login. Unsuccessful. Correct response code.", async () => {
+    it("9. Login. Unsuccessful. Correct response code.", async () => {
         const result = await superagent.post("https://reqres.in/api/login")
         .send(emailOnly)
         .set('Content-Type','application/json')
