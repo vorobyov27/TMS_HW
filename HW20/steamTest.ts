ba9699386572b49ebe42ef383d9a4cb96bfa5616
Alexander <vorobyov27@gmail.com>
2023-01-04 00:31:05 +0300

    fix steam tests

--- HW20/steamTest.ts
+++ HW20/steamTest.ts
@@ -9,12 +9,12 @@ async function newReleasesContent() {
     driver.manage().window().maximize();
     driver.get('https://store.steampowered.com');
     while(!driver.executeScript('document.readyState === "complete"'));
-    const itemAmount = await driver.findElements(By.xpath('//div[@id="tab_newreleases_content"]//a[contains(@href, "app")]'))
+    const items = await driver.findElements(By.xpath('//div[@id="tab_newreleases_content"]//a[contains(@href, "app")]'))
 
-    for (let i = 1; i <= itemAmount.length; i++) {
-        console.log(`${i}.Name: `+await driver.findElement(By.xpath(`//div[@id="tab_newreleases_content"]//a[contains(@href, "app")][${i}]//*[@class="tab_item_name"]`)).getText())
-        console.log("Genres: "+await driver.findElement(By.xpath(`//div[@id="tab_newreleases_content"]//a[contains(@href, "app")][${i}]//*[@class="tab_item_top_tags"]`)).getText())
-        console.log("Price: "+await driver.findElement(By.xpath(`//div[@id="tab_newreleases_content"]//a[contains(@href, "app")][${i}]//*[@class="discount_final_price"]`)).getText())
+    for (let i = 0; i <= items.length; i++) {
+        console.log(`${i}.Name: `+await items[i].findElement(By.xpath(`//*[@class="tab_item_name"]`)).getText())
+        console.log("Genres: "+await items[i].findElement(By.xpath(`//*[@class="tab_item_top_tags"]`)).getText())
+        console.log("Price: "+await items[i].findElement(By.xpath(`//*[@class="discount_final_price"]`)).getText())
         console.log("")
     }
     driver.quit();
